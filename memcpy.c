d4ad4601c2f1fb1d7e79fac4abbc539ae0b391da
Lucas Magasweran <lmagasweran@snapchat.com>
2020-03-25 21:45:13 +0100

    memcpy: add function that optimizes for the architecture and adapt tests to support checking different implementations

--- memcpy.c
+++ memcpy.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-void memcpy_duff(void * __dst, const void * __src, size_t count)
+void *memcpy_duff(void * __dst, const void * __src, size_t count)
 {
     uint8_t * dst = __dst;
     const uint8_t * src = __src;
@@ -24,32 +24,55 @@ void memcpy_duff(void * __dst, const void * __src, size_t count)
     case 1:                 *dst++ = *src++;
             n -= 8; };
     }
+
+    return __dst;
 }
 
-int main()
+void *memcpy_my(void * __dst, const void * __src, size_t count)
+{
+    uint8_t *dst = --__dst;
+    const uint8_t *src = --__src;
+    ssize_t n = count;
+
+    while (count--) // leverage branch-not-equal instructions (checks for zero without immediate)
+        *++dst = *++src; // leverage pre-increment pointer instruction (e.g. PowerPC)
+
+    return __dst;
+}
+
+void test(void *(*memcpy_test)(void * , const void *, size_t))
 {
     uint8_t a[16] = {};
     uint8_t b[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 
     // length < 8
-    memcpy_duff(a, b, 6);
+    memcpy_test(a, b, 6);
     assert(!memcmp(a, b, 6));
     memset(a, 0, sizeof(a));
 
     // length == 8
-    memcpy_duff(a, b, 8);
+    memcpy_test(a, b, 8);
     assert(!memcmp(a, b, 8));
     memset(a, 0, sizeof(a));
 
     // length > 8
-    memcpy_duff(a, b, 10);
+    memcpy_test(a, b, 10);
     assert(!memcmp(a, b, 10));
     memset(a, 0, sizeof(a));
 
     // length == 2*8
-    memcpy_duff(a, b, 2*8);
+    memcpy_test(a, b, 2*8);
     assert(!memcmp(a, b, 2*8));
     memset(a, 0, sizeof(a));
+}
+
+int main()
+{
+    test(&memcpy);
+
+    test(&memcpy_duff);
+
+    test(&memcpy_my);
 
     return 0;
 }
